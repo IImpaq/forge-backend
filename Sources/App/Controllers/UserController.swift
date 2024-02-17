2a36946d3afa985b545cd04bb564a0b6ea0b0484
Marcus Gugacs <iimpaq@proton.me>
2024-02-17 14:24:31 +0100

    Added user functionality controller

---
+++ Sources/App/Controllers/UserController.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Marcus Gugacs on 17.02.24.
+//
+
+import Fluent
+import Vapor
+
+struct UserController: RouteCollection {
+    func boot(routes: RoutesBuilder) throws {
+        
+    }
+}
